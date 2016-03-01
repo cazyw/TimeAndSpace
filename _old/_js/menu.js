8bca1654175205c1aa6297cb3d190edb37e0066f
KC <cazyw@users.noreply.github.com>
2016-03-02 00:20:36 +1100

    add sidebar

--- _old/_js/menu.js
+++ _old/_js/menu.js
@@ -1,19 +1,4 @@
 var main = function() {
-  /* Push the body and the nav over by 200px over */
- /* $('.icon-menu').click(function() {
-    $('.menu').animate({
-      right: "0px"
-    }, 200);
-
-  });*/
-
-  /* Then push them back */
-  /*$('.icon-close').click(function() {
-    $('.menu').animate({
-      right: "-200px"
-    }, 200);
-
-  });*/
   $('.icon-toggle').click(function() {
     $('.menu').toggle();
   });
