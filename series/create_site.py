497f0786feb0a2c3367018ba630a594e19da1bed
KC <cazyw@users.noreply.github.com>
2016-03-02 23:57:48 +1100

    automate series page creation

--- series/create_site.py
+++ series/create_site.py
@@ -3,8 +3,8 @@ import datetime
 # Global variables
 
 rootdir = '.'
-episode_anchor = '<div class="anchor" id="axx"></div>'
-episode_end = '</p>\n\n<!-- episode break -->\n\n'
+episode_anchor = '    <span class="anchor" id="axx"></span>'
+episode_end = '</p>\n\n    </div>\n  </section>\n\n<!-- episode break -->\n\n'
 series_year = {
 	'1': '2005',
 	'2': '2006',
@@ -18,114 +18,111 @@ series_year = {
 	'9': '2015'
 }
 
-hidden_menu = '<li id="xx"><a class="icon-toggle" href="#axx">EPISODE</a></li>\n'
+hidden_menu = '           <li id="xx"><a href="#axx">EPISODE</a></li>\n'
 
-side_menu = '<li><a href="#axx">EPISODE</a></li>\n'
+side_menu = '    <li><a href="#axx">EPISODE</a></li>\n'
+
+section_start = '  <section class="row">\n'
 
 HEADER = '''<!DOCTYPE html>
-<html lang="en">
-  <head>
+<html>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../images/favicon.ico">
-
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Doctor Who Archives - (New) Series x</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet"  type="text/css">
+</head>
 
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-	<link href="css/dashboard.css" rel="stylesheet">
-  </head>
-
-  <body data-spy="scroll" data-target=".bs-docs-sidebar">
-	<!-- begin the top fixed bar -->
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">		        
-          <a class="navbar-brand" href="index.html">Doctor Who Archives</a>
-          <button type="button" class="navbar-toggle icon-toggle">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
+<header>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <!-- Brand -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#episodes" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
           </button>
-		</div>
-				<div class="menu initiallyHidden">
-          <ul class="nav">
-            HIDDEN_MENU
-          </ul>
-		</div>
-
-  	  </div><!--/.navbar-collapse -->
-	
-	<!-- Main jumbotron for the title section -->
-
-	  <div class="jumbotron">
-        <div class="container">
-          <h1>Doctor Who Series x</h1>
-         <p>Episodes from the series in xxxx</p> 
+            <a class="navbar-brand" href="index.html">Doctor Who Archives</a>
         </div>
-      </div>
-    </div> 
-	<!-- end the top fixed bar-->
-	
-	<!-- Fixed Sidebar -->	
- <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar bs-docs-sidebar">
-          <ul class="nav nav-sidebar">
-            SIDEBAR
-          </ul>
-         </div>
-      </div>
- </div>
- 
 
- <!-- /container -->
- <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-    <div class="container-fluid">
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div id="episodes" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+HIDDEN_MENU
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+
+      <div class="jumbotron">
+        <div class="container-fluid">
+          <h1>Doctor Who Series x</h1>
+          <p>Episodes from the series in xxxx</p> 
+        </div><!-- /.container-fluid --> 
+      </div><!-- /.jumbotron -->        
+    </nav> 
+</header>
+
+<body data-spy="scroll" data-target="#episode-list">
+<div id="main-content" class="container-fluid">
+
+
+<!-- sidebar -->
+
+<div id="episode-list" class="sidebar list-group">
+  <ul class="nav nav-sidebar">
+SIDEBAR
+  </ul>
+</div>
+
+<!-- episode review -->
+
+<div class="episode-review">
 '''
 
-FOOTER = '''<!-- Footer -->
-	<footer>
-	<hr>
-	<div class="row">
-		<div class="col-xs-6 col-md-6"><p>&copy; Cazellie YEAR</p></div>
-		<div class="col-xs-6 col-md-6" align="right"><p><a href="acknowledgements.html">Acknowledgements</a></p></div>
-	</div>
-	</footer>
- 
-	</div>
- </div>	
-	
-	
-	
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="js/bootstrap.js"></script>
-	<script src="js/docs.min.js"></script>
-	<script src="js/menu.js"></script>
+FOOTER = '''</div><!-- /.episode review--> 
+</div><!-- /.main content container--> 
 
-	<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+<!-- Footer -->
+<footer>
+    <hr>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-6 col-sm-6 col-md-6"><p>&copy; Cazellie YEAR</p></div>
+            <div class="col-xs-6 col-sm-6 col-md-6" align="right"><p><a href="acknowledgements.html">Acknowledgements</a></p></div>
+        </div>
+    </div>
+</footer>
 
-	ga('create', 'UA-55594701-1', 'auto');
-	ga('send', 'pageview');
-	</script>
-	
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/docs.min.js"></script>
+<script src="js/menu.js"></script>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  </body>
-</html>
+ga('create', 'UA-55594701-1', 'auto');
+ga('send', 'pageview');
+
+
+$(document).ready(function () {
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+});
+</script>
+</body>
+</html> 
 '''
 
 # Traverses the series directories and creates webpage for each series
@@ -163,10 +160,10 @@ for dirName, subdirList, fileList in os.walk(rootdir):
 		
 		local_episode_anchor = episode_anchor.replace('xx', file).replace('.txt', '') + '\n'
 
-		series_input = '<h3 class="page-header">' + current_file.read() 
-		series_working = series_input.replace('\n\n', '</h3>\n<p>',1).replace('\n\n', '</p>\n\n<p>')
+		series_input = '    <div class="col-xs-12">\n      <h3 class="page-header">' + current_file.read() 
+		series_working = series_input.replace('\n\n', '</h3>\n      <p>',1).replace('\n\n', '</p>\n\n      <p>')
 
-		series_output += local_episode_anchor + series_working + episode_end
+		series_output += section_start + local_episode_anchor + series_working + episode_end
 
 	FOOTER = FOOTER.replace('YEAR', str(datetime.datetime.now().year))
 
